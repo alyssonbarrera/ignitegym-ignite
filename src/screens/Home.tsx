17310242ff2d2fd21da070734666dbb5b8ba6128
Alysson Barrera <alyssonbarrera@outlook.com>
2022-11-12 14:45:07 -0300

    :sparkles: add new features

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,86 @@
-import { Center, Text } from 'native-base';
+import { useState } from 'react';
+import { HStack, VStack, FlatList, Heading, Text } from 'native-base';
+
+import { Group } from '@components/Group';
+import { HomeHeader } from '@components/HomeHeader';
+import { ExerciseCard } from '@components/ExerciseCard';
+
+import Image1 from '../assets/image-1.png';
+import Image2 from '../assets/image-2.png';
+import Image3 from '../assets/image-3.png';
+import Image4 from '../assets/image-4.png';
 
 export function Home() {
+
+    const [groupSelected, setGroupSelected] = useState("costas");
+
+    const groups = ["costas", "bíceps", "tríceps", "ombros"];
+    const exercises = [
+        {
+            id: "1",
+            name: "Puxada frontal",
+            image: Image1,
+        },
+        {
+            id: "2",
+            name: "Remada curvada",
+            image: Image2,
+        },
+        {
+            id: "3",
+            name: "Remada unilateral",
+            image: Image3,
+        },
+        {
+            id: "4",
+            name: "Levantamento terra",
+            image: Image4,
+        }
+    ];
+
     return (
-        <Center flex={1}>
-            <Text color="white">Home</Text>
-        </Center>
+        <VStack flex={1}>
+            <HomeHeader />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group
+                        name={item}
+                        isActive={groupSelected.toLocaleUpperCase() ===item.toUpperCase()}
+                        onPress={() => setGroupSelected(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxH={10}
+            />
+            <VStack flex={1} px={8}>
+                <HStack justifyContent="space-between" mb={5}>
+                    <Heading color="gray.200" fontSize="md">
+                        Exercícios
+                    </Heading>
+                    <Text color="gray.200" fontSize="sm">
+                        { exercises.length }
+                    </Text>
+                </HStack>
+
+                <FlatList
+                    data={exercises}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => (
+                        <ExerciseCard
+                            name={item.name}
+                            image={item.image}
+                        />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ pb: 8 }}
+                />
+            </VStack>
+        </VStack>
     );
 }
