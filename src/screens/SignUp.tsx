71936277c28ed36e6e0504ab6d914b1ca74eb905
Alysson Barrera <alyssonbarrera@outlook.com>
2022-11-29 12:47:20 -0300

    :sparkles: add new features

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
+import { Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base';
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup';
 
+import { api } from '@services/api';
+
+import { AppError } from '@utils/AppError';
+
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
-import LogoSvg from '@assets/logo.svg'
+import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 
 type FormDataProps = {
@@ -27,6 +31,8 @@ const signUpSchema = yup.object({
 
 export function SignUp() {
 
+    const toast = useToast();
+
     const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
         resolver: yupResolver(signUpSchema)
     });
@@ -37,8 +43,20 @@ export function SignUp() {
         navigation.goBack();
     };
 
-    function handleSignUp({ name, email, password, password_confirm}: FormDataProps) {
-        console.log({ name, email, password, password_confirm});
+    async function handleSignUp({ name, email, password }: FormDataProps) {
+        try {            
+            const response = await api.post('/users', { name, email, password });
+            console.log(response.data);
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : "Não foi possível criar a conta. Tente novamente mais tarde";
+
+            toast.show({
+                title,
+                placement: "top",
+                bgColor: "red.500",
+            });
+        }
     };
 
     return (
