415415b5008e346d8bff8adfa9a4ea0daa67d85e
Alysson Barrera <alyssonbarrera@outlook.com>
2023-02-04 14:11:44 -0300

    :sparkles: add new features

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -10,6 +10,7 @@ export type AuthContextDataProps = {
     user: UserDTO;
     signIn: (email: string, password: string) => Promise<void>;
     signOut: () => Promise<void>;
+    updateUserProfile: (userUpdated: UserDTO) => Promise<void>;
     isLoadingUserStorageData: boolean;
 };
 
@@ -73,6 +74,15 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
         }
     }
 
+    async function updateUserProfile(userUpdated: UserDTO) {
+        try {
+            setUser(userUpdated);
+            await storageUserSave(userUpdated);
+        } catch (error) {
+            throw error;      
+        }
+    };
+
     async function loadUserData() {
         try {
             setIsLoadingUserStorageData(true);
@@ -95,7 +105,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     }, []);
 
     return (
-        <AuthContext.Provider value={{ user, signIn, signOut, isLoadingUserStorageData }}>
+        <AuthContext.Provider value={{ user, signIn, signOut, isLoadingUserStorageData, updateUserProfile }}>
             { children }
         </AuthContext.Provider>
     )
