5867b61e053111d1feb4bc15cd66b5be0999d1f9
Alysson Barrera <alyssonbarrera@outlook.com>
2023-02-06 13:42:33 -0300

    :sparkles: add new features

---
+++ README.md
@@ -0,0 +1,30 @@
+# Ignite Gym - Ignite | Rocketseat
+
+## Português
+
+#### Aplicativo de acompanhamento de treinos de academia
+
+A aplicação foi desenvolvida em React Native com Expo e NativeBase utilizando o design disponibilizado através do Figma. Nela o usuário pode criar uma conta e fazer login, visualizar exercícios de acordo com a categoria, selecionar um exercício para visualizar detalhes e maneira de executar, visualizar quantidade de séries e repetições, marcar como realizado, visualiar histórico de exercícios realizados e verificar e editar informações do perfil, como foto, nome e senha.
+
+## English
+
+#### Gym workout tracking app
+
+The application was developed in React Native with Expo and NativeBase using the design made available through Figma. In it the user can create an account and log in, view exercises according to the category, select an exercise to view details and how to perform, view number of series and repetitions, mark as performed, view history of exercises performed and check and edit profile information such as photo, name and password.
+
+## Screenshots
+
+<div align="center">
+    <div>
+        <img width="130px" src="./src/assets/print-1.jpg" alt="Print da tela de login" />
+        <img width="130px" src="./src/assets/print-2.jpg" alt="Print da tela de criação de conta" />
+        <img width="130px" src="./src/assets/print-3.jpg" alt="Print da tela inicial onde é possível visualizar exercícios de acordo com a categoria selecionada" />
+        <img width="130px" src="./src/assets/print-4.jpg" alt="Print da tela onde é possível ver os detalhes de um determinado exercício, como quantas séries e repetições fazer, bem como marcar como realizado." />
+        <img width="130px" src="./src/assets/print-5.jpg" alt="Print da tela de histórico, onde é possível visualizar os exercícios feitos de acordo com a data" />
+        <img width="130px" src="./src/assets/print-6.jpg" alt="Print da tela de perfil, onde é possível alterar a foto, nome e senha" />
+    </div>
+</div>
+
+## Stack
+
+<img width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"/> <img width="30px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -5,6 +5,7 @@ import { Heading, VStack } from 'native-base';
 
 import { HistoryCard } from '@components/HistoryCard';
 import { ScreenHeader } from '@components/ScreenHeader';
+import { Loading } from '@components/Loading';
 
 import { ExerciseDTO } from '@dtos/ExerciseDTO';
 import { AppError } from '@utils/AppError';
@@ -46,27 +47,30 @@ export function History() {
         <VStack flex={1}>
             <ScreenHeader title="Histórico de Exercícios" />
 
-            <SectionList
-                sections={ exercises }
-                keyExtractor={(item: ExerciseDTO) => item.id}
-                renderItem={({ item }) => (
-                    <HistoryCard data={ item } />
-                )}
-                renderSectionHeader={({ section }) => (
-                    <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
-                        { section.title }
-                    </Heading>
-                )}
-                px={8}
-                contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
-                ListEmptyComponent={() => (
-                    <Text color="gray.100" textAlign="center">
-                        Ainda não há exercícios registrados. {"\n"}
-                        Vamos treinar hoje?
-                    </Text>
-                )}
-                showsVerticalScrollIndicator={false}
-            />
+            {
+                isLoading ? <Loading /> :
+                    <SectionList
+                        sections={ exercises }
+                        keyExtractor={(item: ExerciseDTO) => item.id}
+                        renderItem={({ item }) => (
+                            <HistoryCard data={ item } />
+                        )}
+                        renderSectionHeader={({ section }) => (
+                            <Heading color="gray.200" fontSize="md" mt={10} mb={3} fontFamily="heading">
+                                { section.title }
+                            </Heading>
+                        )}
+                        px={8}
+                        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+                        ListEmptyComponent={() => (
+                            <Text color="gray.100" textAlign="center">
+                                Ainda não há exercícios registrados. {"\n"}
+                                Vamos treinar hoje?
+                            </Text>
+                        )}
+                        showsVerticalScrollIndicator={false}
+                    />
+            }
         </VStack>
     );
 }
